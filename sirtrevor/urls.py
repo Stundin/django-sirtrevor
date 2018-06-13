4def2e95832e5cfcf708e72234e86eff7fcf8a7e
jonistef <joningi@stundin.is>
2018-06-13 12:24:10 +0000

    Update urls.py

--- sirtrevor/urls.py
+++ sirtrevor/urls.py
@@ -1,14 +1,13 @@
 try:  # pre 1.6
-    from django.conf.urls.defaults import url, patterns
+    from django.conf.urls.defaults import url
 except ImportError:
-    from django.conf.urls import url, patterns
+    from django.conf.urls import url
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(
         '^attachments/',
         'sirtrevor.views.attachment',
         name='sirtrevor_attachments',
     ),
-)
+]
